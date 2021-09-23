0487ef07ec38a2a375dde79248bf2f9f1058a913
freshmanfreshman <slyfoxdumbbunnyz@gmail.com>
2021-09-23 14:35:25 +0900

    ABC216_C

--- ABC/ABC216/C.py
+++ ABC/ABC216/C.py
@@ -1,6 +1,13 @@
 N=int(input())
 
-if N<=120:
-    print("A"*N)
-    exit()
-    
+bin_str = bin(N)
+bin_str = bin_str[2:len(bin_str)]
+
+ans = ""
+for s in bin_str:
+    if s == "0":
+        ans += "B"
+    if s == "1":
+        ans += "BA"
+        
+print(ans)
---
+++ ABC/ABC216/C_test.py
@@ -0,0 +1,23 @@
+
+
+while True:
+
+
+    S = input()
+    ans = 0
+    for s in S:
+        if s=="A":
+            ans += 1
+        else:
+            ans *= 2       
+    print(ans)
+
+
+
+
+
+
+
+
+
+
